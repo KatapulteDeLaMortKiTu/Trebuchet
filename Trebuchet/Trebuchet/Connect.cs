48999ff3ef13f01a0ae55a12ec7dfe9505f9d9c2
Sylouan <sylouan.corfa@epsi.fr>
2019-01-10 18:22:19 +0100

    Get life (intestable, merci l'API)

--- Trebuchet/Trebuchet/Connect.cs
+++ Trebuchet/Trebuchet/Connect.cs
@@ -1,65 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Trebuchet
 {
     class Connect
     {
         private const string URL = "https://dev18504.service-now.com/api/20557/catapulte/getlife2?target=";
-        
+        private const string username = "groupe9";
+        private const string password = "bur8NeG3";
+        private string answer;
+        private StreamReader reader;
 
-        static public int GetLifeAsync(string name)
+        public int GetLifeAsync(string name)
         {
             string link = URL + name;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
+                request.Method = "GET";
+                request.Credentials = new NetworkCredential("groupe9", "bur8NeG3");
+                string credentials = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
-            request.Method = "GET";
-            request.Credentials = new NetworkCredential("groupe9", "bur8NeG3");
+                request.Headers.Add("Authorization", "Basic " + credentials);
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                reader = new StreamReader(((HttpWebResponse)request.GetResponse()).GetResponseStream());
+                answer = reader.ReadToEnd();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message + "L'appel au serveur a échoué");
+            }
+            finally
+            {
+                reader.Close();
+            }
 
-            Stream reception = response.GetResponseStream();
-            StreamReader reader = new StreamReader(reception, Encoding.UTF8);
-            int life = 0;
+            int life = default(int);
 
-
-            //string resultString = reader.ReadToEnd().Split(' ');
-            string test = reader.ReadToEnd();
-            //int life = Convert.ToInt32(reader.ReadToEnd());
-            Console.WriteLine(test);
-
-
-
-            response.Close();
-            reader.Close();
-
-
-            //HttpClient client = new HttpClient
-            //{
-            //    BaseAddress = new Uri(request)
-            //};
-
-            //var values = new Dictionary<string, string> {
-            //    { "Id", "groupe9" },
-            //    {"password", "bur8NeG3"}
-            //};
-            //var content = new FormUrlEncodedContent(values);
-
-
-            //var answer = client.PostAsync(request, content);
-            //int life = Convert.ToInt32(client.GetStringAsync(client.BaseAddress).Result);
+            try
+            {
+                life = GetInt(answer);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             return life;
         }
 
         
+        public int GetInt(string str)
+        {
+            MatchCollection collection = Regex.Matches(str, @"\b\d+\b");
 
+            List<int> ints = new List<int>();
+            int result;
+            foreach(Match match in collection)
+            {
+                result = int.Parse(match.Value);
+                ints.Add(result);
+            }
+
+            result = 0;
+
+            for(int i = 0; i < ints.Count; i++)
+            {
+                result += ints[i] * (ints.Count - i);
+            }
+
+            return result;
+        }
     }
 }
