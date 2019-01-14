7e95b6bd5357a9f31bf1420a2f73af770950991a
Unknown <maxime.monnier@epsi.fr>
2019-01-14 12:47:39 +0100

    Revert "Merge remote-tracking branch 'origin/maxime'"
    This reverts commit 467cec1f055632b7fb6f78361f8e98e2f4ef8b41.

---
+++ Trebuchet/Trebuchet/Rock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trebuchet
+{
+    public class Rock
+    {
+
+        public int width;
+        public int weight;
+
+        public Rock(int width, int weight)
+        {
+            this.width = width;
+            this.weight = weight;
+        }
+
+
+    }
+}
