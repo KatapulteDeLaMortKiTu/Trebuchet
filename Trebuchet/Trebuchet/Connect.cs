fd6dd2f11c5e7693859ce01a7c26400b67d9c231
Sylouan <sylouan.corfa@epsi.fr>
2019-01-15 18:14:12 +0100

    Fix requête Fire

--- Trebuchet/Trebuchet/Connect.cs
+++ Trebuchet/Trebuchet/Connect.cs
@@ -72,17 +72,27 @@ namespace Trebuchet
 
 
                 //Création de la requête avec les bons credentials
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://dev18504.service-now.com/api/20557/catapulte/attack?power=" + power + "&target=" + target);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://dev18504.service-now.com/api/20557/catapulte/fire?power=" + power + "&target=" + target);
                 request.Method = "POST";
 
                 string credentials = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
 
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                Stream receiveStream = response.GetResponseStream();
-                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
-                Console.WriteLine(readStream.ReadToEnd());
-
                 request.Headers.Add("Authorization", "Basic " + credentials);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Console.WriteLine("Content length is {0}", response.ContentLength);
+                Console.WriteLine("Content type is {0}", response.ContentType);
+
+                // Get the stream associated with the response.
+                Stream receiveStream = response.GetResponseStream();
+
+                // Pipes the stream to a higher level stream reader with the required encoding format. 
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+                Console.WriteLine("Response stream received.");
+                Console.WriteLine(readStream.ReadToEnd());
+                response.Close();
+                readStream.Close();
             }
             catch(Exception e)
             {
