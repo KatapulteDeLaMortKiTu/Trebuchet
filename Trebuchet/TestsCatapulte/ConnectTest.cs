88557882e773cdadc4f84fc5916a6ff272cc08d0
Sylouan <sylouan.corfa@epsi.fr>
2019-01-15 23:10:06 +0100

    Début Tests

---
+++ Trebuchet/TestsCatapulte/ConnectTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsCatapulte
+{
+    [TestClass]
+    public class ConnectTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
